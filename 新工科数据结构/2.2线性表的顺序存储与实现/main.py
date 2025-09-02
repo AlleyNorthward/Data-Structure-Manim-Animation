5481f422405df29a681c55949789dddac7a44f15
AlleyNorthward <2417534000@qq.com>
2025-09-02 09:33:29 +0800

    Add files via upload

---
+++ 新工科数据结构/2.2线性表的顺序存储与实现/main.py
@@ -0,0 +1,92 @@
+from manim import*
+from src.FirstVideo import FirstScene
+from src.FirstVideo import SecnodScene
+from src.FirstVideo import ThirdScene
+from src.FirstVideo import FourthScene
+from src.FirstVideo import FifthScene
+from src.FirstVideo import SixthScene
+from src.FirstVideo import SevenScene
+from src.FirstVideo import EightScene
+from src.FirstVideo import NineScene
+from src.FirstVideo import TenScene
+from src.FirstVideo import ElevenScene
+from src.SecondChapterVideo import SecondChapterScene
+
+class First1(FirstScene):
+    def construct(self):
+        return super().construct()
+    
+class Second2(SecnodScene):
+    def construct(self):
+        return super().construct()
+
+class Third3(ThirdScene):
+    def construct(self):
+        return super().construct()
+    
+class Fourth4(FourthScene):
+    def construct(self):
+        return super().construct()
+    
+class Fifth5(FifthScene):
+    def construct(self):
+        return super().construct()
+    
+class Sixth6(SixthScene):
+    def construct(self):
+        return super().construct()
+
+class Seven7(SevenScene):
+    def construct(self):
+        return super().construct()
+
+class Eight8(EightScene):
+    def construct(self):
+        return super().construct()
+
+class Nine9(NineScene):
+    def construct(self):
+        return super().construct()
+
+class Ten10(TenScene):
+    def construct(self):
+        return super().construct()
+
+class Eleven11(ElevenScene):
+    def construct(self):
+        return super().construct()
+    
+class Chapter(SecondChapterScene):
+    def construct(self):
+        return super().construct()
+
+class Chapter2(SecondChapterScene):
+    def construct(self):
+        return super().construct()
+
+class Chapter3(SecondChapterScene):
+    def construct(self):
+        return super().construct()
+    
+class Chapter4(SecondChapterScene):
+    def construct(self):
+        return super().construct()
+
+class Chapter5(SecondChapterScene):
+    def construct(self):
+        return super().construct()
+
+class Chapter6(SecondChapterScene):
+    def construct(self):
+        return super().construct()
+    
+class Chapter7(SecondChapterScene):
+    def construct(self):
+        return super().construct()
+    
+class Chapter8(SecondChapterScene):
+    def construct(self):
+        return super().construct()
+
+
+
